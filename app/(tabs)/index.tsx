dda48c9930b404854316732e62e757f0eef6232f
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-06-10 14:19:14 -0300

    feat: add content section layout for listing items on home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,10 +9,12 @@ export default function App() {
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-      <View style={styles.content}>
+      <View style={styles.form}>
         <Input placeholder="O que você precisa comprar?" />
         <Button title="Entrar" />
       </View>
+
+      <View style={styles.content}></View>
     </View>
   );
 }
--- components/styles/tabs.styles.ts
+++ components/styles/tabs.styles.ts
@@ -3,19 +3,27 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#d0d2d8",
     alignItems: "center",
-    gap: 10,
+    backgroundColor: "#d0d2d8",
     padding: 10,
   },
   logo: {
     height: 34,
     width: 134,
-    justifyContent: "center",
+  },
+  form: {
+    width: "100%",
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42,
   },
   content: {
+    flex: 1,
     width: "100%",
-    paddingHorizontal: 24,
-    gap: 10,
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    marginTop: 24,
   },
 });
