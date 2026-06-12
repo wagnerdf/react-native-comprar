a48df94e0e91da5c5f9b705f9a8fd2018b94dac5
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-06-12 15:21:23 -0300

    feat: add async storage for local data persistence

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,12 @@
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import {
+  Alert,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import { Button } from "@/components/Button";
 import { Filter } from "@/components/Filter";
@@ -9,47 +17,44 @@ import { styles } from "@/components/styles/tabs.styles";
 import { FilterStatus } from "@/types/FilterStatus";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
-const ITEMS = [
-  { id: "1", status: FilterStatus.DONE, description: "1 pacote de cafe" },
-  { id: "2", status: FilterStatus.PENDING, description: "2 pacote de feijão" },
-  { id: "3", status: FilterStatus.PENDING, description: "1 lata de oleo" },
-  { id: "4", status: FilterStatus.DONE, description: "3 pacotes de macarrão" },
-  { id: "5", status: FilterStatus.PENDING, description: "2 pacote de feijão" },
-  { id: "6", status: FilterStatus.PENDING, description: "1 lata de oleo" },
-  { id: "7", status: FilterStatus.DONE, description: "1 pacote de cafe" },
-  { id: "8", status: FilterStatus.PENDING, description: "2 pacote de feijão" },
-  { id: "9", status: FilterStatus.PENDING, description: "1 lata de oleo" },
-  { id: "10", status: FilterStatus.DONE, description: "3 pacotes de macarrão" },
-  { id: "11", status: FilterStatus.PENDING, description: "2 pacote de feijão" },
-  { id: "12", status: FilterStatus.PENDING, description: "1 lata de oleo" },
-  { id: "13", status: FilterStatus.DONE, description: "1 pacote de cafe" },
-  { id: "14", status: FilterStatus.PENDING, description: "2 pacote de feijão" },
-  { id: "15", status: FilterStatus.PENDING, description: "1 lata de oleo" },
-  { id: "16", status: FilterStatus.DONE, description: "3 pacotes de macarrão" },
-  { id: "17", status: FilterStatus.PENDING, description: "2 pacote de feijão" },
-  { id: "18", status: FilterStatus.PENDING, description: "1 lata de oleo" },
-  { id: "19", status: FilterStatus.DONE, description: "1 pacote de cafe" },
-  { id: "20", status: FilterStatus.PENDING, description: "2 pacote de feijão" },
-  { id: "21", status: FilterStatus.PENDING, description: "1 lata de oleo" },
-  { id: "22", status: FilterStatus.DONE, description: "3 pacotes de macarrão" },
-  { id: "23", status: FilterStatus.PENDING, description: "2 pacote de feijão" },
-  { id: "24", status: FilterStatus.PENDING, description: "1 lata de oleo" },
-];
 
 export default function App() {
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+  const [description, setDescription] = useState("");
+  const [items, setItems] = useState<any>([]);
+
+  function handleAdd() {
+    if (!description.trim()) {
+      return Alert.alert("Adicionar", "Informe a descrição para adicionar.");
+    }
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING,
+    };
+  }
+
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
       <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
-        <Button title="Entrar" />
+        <Input
+          placeholder="O que você precisa comprar?"
+          onChangeText={setDescription}
+        />
+        <Button title="Adicionar" onPress={handleAdd} />
       </View>
 
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map((status) => (
-            <Filter key={status} status={status} isActive />
+            <Filter
+              key={status}
+              status={status}
+              isActive={status === filter}
+              onPress={() => setFilter(status)}
+            />
           ))}
 
           <TouchableOpacity style={styles.clearButton}>
@@ -58,7 +63,7 @@ export default function App() {
         </View>
 
         <FlatList
-          data={ITEMS}
+          data={items}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
             <Item
